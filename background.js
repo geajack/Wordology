52d805015a9e47cbfd1af06fdf79271d6e0a7370
Jack <babybishops@gmail.com>
2019-03-24 13:08:58 +0100

    Fix formatting

--- background.js
+++ background.js
@@ -1,41 +1,37 @@
-(async function BackgroundScript()
-{
-	var OM = new OptionsManager();
-	await OM.initializeStorage();
+(async function BackgroundScript() {
+		var OM = new OptionsManager();
+		await OM.initializeStorage();
 
-	var TM = new ToggleManagerBackground("ToggleManager",
-		{
-			notReadyIcon : "./icons/icon_inactive_32.png",
-			offIcon      : "./icons/icon_off_32.png",
-			runningIcon  : "./icons/icon_inactive_32.png",
-			onIcon       : "./icons/icon_on_32.png"
-		}
-	);
+		var TM = new ToggleManagerBackground("ToggleManager",
+			{
+				notReadyIcon: "./icons/icon_inactive_32.png",
+				offIcon: "./icons/icon_off_32.png",
+				runningIcon: "./icons/icon_inactive_32.png",
+				onIcon: "./icons/icon_on_32.png"
+			}
+		);
 
-	var profileId = await OM.getCurrentProfileId();
-    var DF = new DictionaryFetcherBackground("DictionaryFetcher");
-	OM.addOnChangeProfileListener(onChangeProfile);
+		var profileId = await OM.getCurrentProfileId();
+		var DF = new DictionaryFetcherBackground("DictionaryFetcher");
+		OM.addOnChangeProfileListener(onChangeProfile);
 
-	if (profileId !== null)
-	{
-		DF.setDictionary(new Dictionary(profileId));
-	}
-	else
-	{
-		TM.loggedOut();
-	}
-
-    function onChangeProfile(profileId)
-    {
-		if (profileId !== null)
-		{
-			console.log("Profile changed to " + profileId);
+		if (profileId !== null) {
 			DF.setDictionary(new Dictionary(profileId));
-			TM.loggedIn();
 		}
-		else
-		{
+		else {
 			TM.loggedOut();
 		}
-    }
+
+		function onChangeProfile(profileId) {
+			if (profileId !== null)
+			{
+				console.log("Profile changed to " + profileId);
+				DF.setDictionary(new Dictionary(profileId));
+				TM.loggedIn();
+			}
+			else
+			{
+				TM.loggedOut();
+			}
+		}
 })();
