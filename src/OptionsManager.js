29a7590635d6fbeed028783f90fa63d6836dbe2f
Jack <->
2019-11-30 10:47:50 +0100

    Add menu in options to change language

--- src/OptionsManager.js
+++ src/OptionsManager.js
@@ -52,7 +52,7 @@ class OptionsManager
             (changes, storageAreaName) => {
                 if (storageAreaName === "local")
                 {
-                    if (changes.profile !== undefined)
+                    if (changes.profile.newValue !== changes.profile.oldValue)
                     {
                         listener(changes.profile.newValue);
                     }
